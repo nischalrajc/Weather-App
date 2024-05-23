42718742d5cfc060de5c8f0fa896de9d394b9be5
nischalrajc <nischalraj22@gmail.com>
2024-05-23 19:21:40 +0530

    temperature component rendered dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Highlights from './components/Highlights'
 import Temperature from './components/Temperature'
 
 function App() {
+  const [city, setCity] = useState("Kerala")
+  const [weatherData, setWeatherData] = useState(null)
+  const apiURL = `https://api.weatherapi.com/v1/current.json?key=b52138930e784f41929125748242305&q=${city}&aqi=no`
+
+
+  useEffect(() => {
+
+    fetch(apiURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+        setWeatherData(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [city])
+
 
   return (
     <h1 className="bg-blue-950 h-screen flex justify-center">
 
       <div className="mt-40 w-1/5 h-1/3">
-        <Temperature />
+        <Temperature
+          setCity={setCity}
+          stats={{
+            temp: weatherData?.current?.temp_c,
+            condition: weatherData?.current?.condition?.text,
+            isDay: weatherData?.current?.is_day,
+            location: weatherData?.location?.name,
+            time: weatherData?.location?.localtime
+          }}
+        />
       </div>
 
       <div className='w-1/3 h-1/3 p-10 mt-40 grid grid-cols-2 gap-6'>
