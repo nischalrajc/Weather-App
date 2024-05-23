cdebe63912bb52a1eb9ce154011dedf8493a82b5
nischalrajc <nischalraj22@gmail.com>
2024-05-23 18:23:14 +0530

    created two component ,tailwind configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Highlights from './components/Highlights'
+import Temperature from './components/Temperature'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1 className="bg-blue-950 h-screen flex justify-center">
+
+      <div className="mt-40 w-1/5 h-1/3">
+        <Temperature />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='w-1/3 h-1/3 p-10 mt-40 grid grid-cols-2 gap-6'>
+        <h2 className='text-slate-200 col-span-2'>Today's Highlights</h2>
+        <Highlights />
+        <Highlights />
+        <Highlights />
+        <Highlights />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </h1>
   )
 }
 
